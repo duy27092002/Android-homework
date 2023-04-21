f7eabbb221813dff66e76e264a2c0c468095e239
dkifashion <dkiwomanfashion@gmail.com>
2023-04-21 20:21:20 +0700

    additional type converter

--- NoteV3/app/src/main/java/t3h/android/notev3/converters/Converters.java
+++ NoteV3/app/src/main/java/t3h/android/notev3/converters/Converters.java
@@ -1,2 +1,17 @@
-package t3h.android.notev3.converters;public class Converters {
+package t3h.android.notev3.converters;
+
+import androidx.room.TypeConverter;
+
+import java.sql.Timestamp;
+
+public class Converters {
+    @TypeConverter
+    public static Timestamp fromTimestamp(Long value) {
+        return value == null ? null : new Timestamp(value);
+    }
+
+    @TypeConverter
+    public static Long dateToTimestamp(Timestamp date) {
+        return date == null ? null : date.getTime();
+    }
 }
--- NoteV3/app/src/main/java/t3h/android/notev3/database/NoteRoomDatabase.java
+++ NoteV3/app/src/main/java/t3h/android/notev3/database/NoteRoomDatabase.java
@@ -5,11 +5,14 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
 
+import t3h.android.notev3.converters.Converters;
 import t3h.android.notev3.dao.NoteDao;
 import t3h.android.notev3.entities.Note;
 
 @Database(entities = Note.class, version = 1, exportSchema = false)
+@TypeConverters({Converters.class})
 public abstract class NoteRoomDatabase extends RoomDatabase {
     public abstract NoteDao noteDao();
 
@@ -21,5 +24,6 @@ public abstract class NoteRoomDatabase extends RoomDatabase {
                     Room.databaseBuilder(context, NoteRoomDatabase.class, "note_db.db").build();
         }
         return INSTANCE;
+
     }
 }
